50296d5af9d2d1dcec163ddca36da3ac9fb0ac1c
Vanderlei <vanderleijr2001@gmail.com>
2024-09-13 18:56:03 -0700

    Initial Commit

---
+++ LAB-04Lab04 – Change in Euro Coins • • • • • Lab04 – Change in Euro Coins • • • • • LAB04 - eurocoin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LAB-04Lab04 – Change in Euro Coins • • • • • Lab04 – Change in Euro Coins • • • • • LAB04 - eurocoin
+//
+//  Created by Vanderlei Junior on 9/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
